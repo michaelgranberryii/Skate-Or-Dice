2332e527ab1903ebc58585c6d0528811a735ac9e
Michael Granberry <gberry03@me.com>
2021-02-11 23:25:35 -0800

    Initial Commit

---
+++ Skate-Or-Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skate-Or-Dice
+//
+//  Created by Michael Granberry II on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
