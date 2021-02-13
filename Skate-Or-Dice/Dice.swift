9615cd5d25d6d73da3489e4aaf9749af5a9325c4
Michael Granberry <gberry03@me.com>
2021-02-12 21:42:51 -0800

    Add image literal comment

--- Skate-Or-Dice/Dice.swift
+++ Skate-Or-Dice/Dice.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class Dice {
-    let orientation : Array = [#imageLiteral(resourceName: "Frontside"),#imageLiteral(resourceName: "Backside"),#imageLiteral(resourceName: "Skate Or Dice"),#imageLiteral(resourceName: "Skateboards")]
+    let orientation : Array = [#imageLiteral(resourceName: "Frontside"),#imageLiteral(resourceName: "Backside"),#imageLiteral(resourceName: "Skate Or Dice"),#imageLiteral(resourceName: "Skateboards")] // image literals
     let stance : Array =      [#imageLiteral(resourceName: "Ollie"),#imageLiteral(resourceName: "Fakie"),#imageLiteral(resourceName: "Switch"),#imageLiteral(resourceName: "Nollie"),#imageLiteral(resourceName: "Skate Or Dice"),#imageLiteral(resourceName: "Skateboards")]
     let spin : Array =        [#imageLiteral(resourceName: "360"),#imageLiteral(resourceName: "180"),#imageLiteral(resourceName: "0"),#imageLiteral(resourceName: "Skate Or Dice"),#imageLiteral(resourceName: "Skateboards")]
     let flip : Array =        [#imageLiteral(resourceName: "Kickflip"),#imageLiteral(resourceName: "Heelflip"),#imageLiteral(resourceName: "Skateboards"),#imageLiteral(resourceName: "Skate Or Dice")]
